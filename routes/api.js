37fa920b36d9822777ff7cd475bd94c5dd34b511
Replit user <>
2024-02-10 10:44:02 +0000

    helmet

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,22 @@
 'use strict';
 
+import helmet from 'helmet';
+
+const express = require('express');
+const helmet = require('helmet');
+
+const app = express();
+
+app.use(helmet({
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'"],
+      styleSrc: ["'self'"]
+    }
+  }
+}));
+
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
