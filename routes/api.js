425b41eada1b3c1394ae6bad91282fe77cbd3a12
Replit user <>
2024-02-19 12:02:39 +0000

    fix handle arrays of stocks

--- routes/api.js
+++ routes/api.js
@@ -18,33 +18,32 @@ function anonymize(ip) {
   return hash.digest('hex').substring(0, 7);
 }
 
-async function getStockPrice(stock, like, ip, callback) {
+async function getStockPrice(stocks, like, ip, callback) {
   try {
-    const response = await axios.get(`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock}/quote`);
-    const price = response.data.latestPrice;
-    const stockData = {
-      stock: stock,
-      price: price,
-      likes: like ? 1 : 0
-    };
-    if (like) {
-      const existingStock = await Stock.findOne({ symbol: stock });
-      if (existingStock) {
-        const existingIpIndex = existingStock.ips.indexOf(ip);
-        if (existingIpIndex === -1) {
-          existingStock.ips.push(ip);
-          existingStock.likes += 1;
-          await existingStock.save();
+    const stockData = await Promise.all(stocks.map(async (stock) => {
+      const response = await axios.get(`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock}/quote`);
+      const price = response.data.latestPrice;
+      let likes = like ? 1 : 0;
+      if (like) {
+        const existingStock = await Stock.findOne({ symbol: stock });
+        if (existingStock) {
+          const existingIpIndex = existingStock.ips.indexOf(ip);
+          if (existingIpIndex === -1) {
+            existingStock.ips.push(ip);
+            existingStock.likes += 1;
+            await existingStock.save();
+          }
+        } else {
+          const newStock = new Stock({
+            symbol: stock,
+            ips: [ip],
+            likes: 1
+          });
+          await newStock.save();
         }
-      } else {
-        const newStock = new Stock({
-          symbol: stock,
-          ips: [ip],
-          likes: 1
-        });
-        await newStock.save();
       }
-    }
+      return {stock, price, likes};
+    }));
     callback(null, stockData);
   } catch (error) {
     callback(error, null);
@@ -56,14 +55,17 @@ module.exports = {
   setupRoutes: function(app) {
     app.route('/api/stock-prices')
        .get(function (req, res) {
-          const stock = req.query.stock;
+          let stocks = req.query.stock;
+          if (!Array.isArray(stocks)) {
+            stocks = [stocks];
+          }
           const like = req.query.like;
           const ip = anonymize(req.ip);
-        getStockPrice(stock, like, ip, (err, data) => {
-          if (err) {
-            res.json({error: 'Stock not found'});
-          } else {
-            res.json({stockData: {stock: data.stock, price: data.price, likes: data.likes}});
+          getStockPrice(stocks, like, ip, (err, data) => {
+            if (err) {
+              res.json({error: 'Stock not found'});
+            } else {
+              res.json({stockData: data});
           }
         });
       });
